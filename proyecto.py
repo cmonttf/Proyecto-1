6706822b7ee26f10f9e04afd637b115380bef5d2
JuanBaezaB <51477488+JuanBaezaB@users.noreply.github.com>
2022-09-12 10:14:23 -0300

    actualizacion

--- proyecto.py
+++ proyecto.py
@@ -1,2 +1,38 @@
-tamano_tablero = 10
-semilla = 5
+#*********************B*I*B*L*I*O*T*E*C*A*S********************
+
+from ctypes import sizeof
+import random
+
+#**************************************************************
+
+#**********************F*U*N*C*I*O*N*E*S***********************
+
+#Función randomica que nos da los valores alelos del gen
+def rand_bin(n):
+    vector = []
+    for i in range(n):
+        vector.append(random.randint(1, n))
+    
+    return vector
+
+#Función fitness que nos da el valor de que tan cercano a lo ideal estamos
+def fitness(vector):
+    tamanio = len(vector)
+    return tamanio
+
+#**************************************************************
+
+#****************************M*A*I*N***************************
+
+#Solicitamos los datos por pantalla
+print("Ingrese los datos: (tamaño gen, tamaño poblacion)")
+n_gen = int(input())
+n_pobl = int(input())
+
+
+for i in range(n_pobl):
+    vector = rand_bin(n_gen)
+    print(rand_bin(n_gen))
+    print(fitness(vector))
+
+#**************************************************************

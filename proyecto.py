5a5729e840cc3aa2bf4d3d3fa59757163c44aa71
nicolas-122 <73654969+nicolas-122@users.noreply.github.com>
2022-09-29 05:53:13 -0300

    cruza

--- proyecto.py
+++ proyecto.py
@@ -2,7 +2,10 @@
 
 from ctypes import sizeof
 from operator import index
+from pickle import TRUE
 import random as rd
+from traceback import print_tb
+from xmlrpc.client import boolean
 import numpy as np
 import sys
 import time
@@ -35,7 +38,7 @@ def ini_poblacion(n, p):
 #Función fitness que nos da el valor de que tan cercano a lo ideal estamos, calculo de coliciones
 def fitness(pob):
     #max_choques=56
-    tamanio=pob.shape
+    tamanio=np.shape(pob)
     p=tamanio[0] #poblacion
     n=tamanio[1] #tamaño tablero
     fit=np.zeros(p,dtype=int) #vector final que nos dara el valor de colicion de cada tablero
@@ -48,18 +51,29 @@ def fitness(pob):
                     cont=cont+1
                       
             if((n-1)==i):
+
                 fit[k]=cont
+                if(fit[k]==0):
+                    print("AAAAA")
                 cont=0
+
+    
 
     return fit
 
 #Seleccion de cruzamiento
 def seleccion(v_fit,m_pob,prob_cruza):
+    nuevo_fitness=np.zeros(np.size(v_fit))
 
-    nuevo_fitness=np.ones(np.size(v_fit))/v_fit/(np.ones(np.size(v_fit))*np.sum(v_fit)) #nuevo fitness (1/fitness/sum(fitness))
-    nuevo_proporcion=nuevo_fitness/(np.ones(np.size(v_fit))*np.sum(nuevo_fitness))      #nueva proporcion (nuevo fitness/sum(nuevo fitness))
+    for i in range(np.size(v_fit)):
+        if(v_fit[i]==0):
+            v_fit[i]=1
+        nuevo_fitness[i]=1/v_fit[i]/np.sum(v_fit)
+
+    nuevo_proporcion=nuevo_fitness/(np.ones(np.size(nuevo_fitness))*np.sum(nuevo_fitness)) 
     nuevo_ruleta=np.zeros(np.size(nuevo_proporcion))                                    #nueva ruleta
-    cont=0                                                                   
+    cont=0                      
+                                
 
     for i in range(np.size(nuevo_proporcion)):
         if(i==0):
@@ -69,8 +83,35 @@ def seleccion(v_fit,m_pob,prob_cruza):
         
         if(nuevo_ruleta[i]<(prob_cruza/100)):
             cont=cont+1
-        
+  
     return m_pob[0:cont]
+
+#Cruzar dos individuos con un punto de cruza.
+def cruza(pobl):
+    dim=np.shape(pobl)
+    p=dim[0]
+    n=dim[1]
+    ind=np.zeros(2,dtype=int)
+    con_ind=np.zeros(2,dtype=boolean)
+    cruzados=np.zeros(p,dtype=boolean)
+    punto_cruza=valor = np.random.randint(1,n)
+    
+    for i in range(2):
+        while(con_ind[i]==False):
+            ind[i]=np.random.randint(p)
+            if(cruzados[ind[i]]==False):
+                cruzados[ind[i]]=TRUE
+                con_ind[i]=TRUE
+
+    print(ind)
+    print(punto_cruza)
+    print(pobl[ind[0]][0:punto_cruza])
+    print(pobl[ind[0]][punto_cruza:n])
+        
+    return 0
+
+
+
 #**************************************************************
 
 #****************************M*A*I*N***************************
@@ -121,6 +162,7 @@ mimatriz = ini_poblacion(n, p)
 print(mimatriz,"\n")
 mifitness = fitness(mimatriz)
 mimatriz =seleccion(mifitness,mimatriz,prov_c)
-print(mimatriz)
+print(mimatriz,"\n")
 
+cruza(mimatriz)
 #**************************************************************

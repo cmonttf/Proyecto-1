fdd33191fbc4f607ab7908275bd9ce462a46a7f9
nicolas-122 <73654969+nicolas-122@users.noreply.github.com>
2022-09-29 01:53:28 -0300

    edit final seleccion

--- proyecto.py
+++ proyecto.py
@@ -1,6 +1,7 @@
 #*********************B*I*B*L*I*O*T*E*C*A*S********************
 
 from ctypes import sizeof
+from operator import index
 import random as rd
 import numpy as np
 import sys
@@ -12,12 +13,12 @@ import time
 
 #Función randomica entre 0 y 1
 def rand_bin():
-    valor = rd.randint(0, 1)
+    valor = np.random.randint(0, 1)
     return valor
 
 #Función randomico entre 1 y n
 def rand_n(n):
-    valor = rd.randint(0,(n-1))
+    valor = np.random.randint(0,n)
     return valor
 
 #Funcion incializacion poblacion
@@ -25,8 +26,8 @@ def ini_poblacion(n, p):
     matriz = np.zeros((p,n), dtype=int)
     
     for i in range(p):
-        for j in range(n):
-            matriz[i][j] = rand_n(n)
+        matriz[i] = np.arange(n)
+        np.random.shuffle(matriz[i])
     
     return matriz
      
@@ -37,45 +38,39 @@ def fitness(pob):
     tamanio=pob.shape
     p=tamanio[0] #poblacion
     n=tamanio[1] #tamaño tablero
-    fit=[] #vector final que nos dara el valor de colicion de cada tablero
+    fit=np.zeros(p,dtype=int) #vector final que nos dara el valor de colicion de cada tablero
     cont=0
 
     for k in range(p):
         for i in range(n):
             for j in range(n):
-                if(i!=j):
-                    if(pob[k][i]==pob[k][j]): #cuenta los choques en horizontal
-                        cont=cont+1
-
-                    if(abs(i-j)==abs(pob[k][i]-pob[k][j])): #cuenta choques en diagonal
-                        cont=cont+1
-
+                if((abs(i-j)==abs(pob[k][i]-pob[k][j]))&(i!=j)): #cuenta choques en diagonal
+                    cont=cont+1
+                      
             if((n-1)==i):
-                fit.append(cont)
+                fit[k]=cont
                 cont=0
 
     return fit
 
-#seleccion de cruzamiento
+#Seleccion de cruzamiento
 def seleccion(v_fit,m_pob,prob_cruza):
-    #print("suma fit= ",np.sum(v_fit))
+
     nuevo_fitness=np.ones(np.size(v_fit))/v_fit/(np.ones(np.size(v_fit))*np.sum(v_fit)) #nuevo fitness (1/fitness/sum(fitness))
     nuevo_proporcion=nuevo_fitness/(np.ones(np.size(v_fit))*np.sum(nuevo_fitness))      #nueva proporcion (nuevo fitness/sum(nuevo fitness))
-    nuevo_ruleta=[]                                                                     #nueva ruleta
+    nuevo_ruleta=np.zeros(np.size(nuevo_proporcion))                                    #nueva ruleta
+    cont=0                                                                   
 
     for i in range(np.size(nuevo_proporcion)):
         if(i==0):
-            nuevo_ruleta.append(nuevo_proporcion[i])
+            nuevo_ruleta[i]=nuevo_proporcion[i]
         else:
-            nuevo_ruleta.append(nuevo_ruleta[(i-1)]+nuevo_proporcion[i])
-
-    #print("nuevo fitnes = ",nuevo_fitness)
-    #print("nuevo proporcion = ",nuevo_proporcion)
-    #print("suma total de nuevo fitnes = ",np.sum(nuevo_fitness))
-    #print("suma total de nuevo proporcion = ",np.sum(nuevo_proporcion))
-    #print("nueva ruleta = ",nuevo_ruleta)
-    a=0
-    return a
+            nuevo_ruleta[i]=nuevo_ruleta[(i-1)]+nuevo_proporcion[i]
+        
+        if(nuevo_ruleta[i]<(prob_cruza/100)):
+            cont=cont+1
+        
+    return m_pob[0:cont]
 #**************************************************************
 
 #****************************M*A*I*N***************************
@@ -87,14 +82,15 @@ if len(sys.argv) == 5:
     n = int(sys.argv[2])
     p = int(sys.argv[3])
     prov_c = int(sys.argv[4])
-    print("semilla             = ",semilla)
+    print("semilla            = ",semilla)
     print("tamaño de tablero   = ", n)
     print("tamaño de poblacion = ", p)
 else:
     print("Error")
     print("Ingrese los parametros correctos: ''py proyecto.py semilla tamaño_tablero tamaño_poblacion probabilidad de cruza''")
+#np.random.seed(semilla)
 
-
+'''
 inicio = time.time()
 np.random.seed(semilla)
     
@@ -118,10 +114,13 @@ while encontrado == 0:
             print("****************************************")
             print("****************************************")
             print()
+'''
 
-    #print(mimatriz)
-    #print(mifitness)
-
-    seleccion(mifitness,mimatriz,prov_c)
+#print(mifitness)
+mimatriz = ini_poblacion(n, p)
+print(mimatriz,"\n")
+mifitness = fitness(mimatriz)
+mimatriz =seleccion(mifitness,mimatriz,prov_c)
+print(mimatriz)
 
 #**************************************************************
